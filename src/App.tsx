fdacf5a669b38c1fa0e5ef216057489120c3bc5f
Beamlak996 <beamlaktsegaye996@gmail.com>
2023-10-18 08:42:08 -0900

    header-finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
+import {PageHeader} from "./layouts/PageHeader"
 
 function App() {
 
   return (
-    <div>
-      Hello There
+    <div className="max-h-screen flex flex-col" >
+      <PageHeader />
+      <div className="grid grid-cols-[auto,1fr] flex-grow-1 overflow-auto" >
+        <div>Sidebar</div>
+        <div></div>
+      </div>
     </div>
   )
 }
