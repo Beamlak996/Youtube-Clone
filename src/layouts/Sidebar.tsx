524c8d0d561e858d4446112f866b89f339f7bb89
Beamlak996 <beamlaktsegaye996@gmail.com>
2023-10-21 01:28:35 -0900

    Large Sidebar Complete

--- src/layouts/Sidebar.tsx
+++ src/layouts/Sidebar.tsx
@@ -1,8 +1,134 @@
+import {
+  ChevronDown,
+  ChevronUp,
+  Clapperboard,
+  Home,
+  Library,
+  Repeat,
+} from "lucide-react";
+import { Children, ElementType, useState } from "react";
+import Button, { buttonStyles } from "../components/Button";
+import { twMerge } from "tailwind-merge";
 
 const Sidebar = () => {
   return (
-    <aside className="stick top-0 overflow-y-auto" ></aside>
-  )
+    <>
+      <aside className="sticky top-0 overflow-y-auto scrollbar-hidden pb-4 flex flex-col ml-1 lg:hidden">
+        <SmallSidebarItem Icon={Home} title="Home" url="/" />
+        <SmallSidebarItem Icon={Repeat} title="Shorts" url="/shorts" />
+        <SmallSidebarItem
+          Icon={Clapperboard}
+          title="Subscriptions"
+          url="/subscriptios"
+        />
+        <SmallSidebarItem Icon={Library} title="Library" url="/library" />
+      </aside>
+      <aside className="w-56 lg:sticky absolute top-0 overflow-y-auto scrollbar-hidden pb-4 flex-col gap-2 px-2 flex">
+        <LargeSidebarSection>
+          <LargeSidebarItem isActive Icon={Home} title="Home" url="/" />
+          <LargeSidebarItem
+            Icon={Clapperboard}
+            title="Subscriptions"
+            url="/subscriptios"
+          />
+        </LargeSidebarSection>
+        <hr />
+        <LargeSidebarSection>
+          
+        </LargeSidebarSection>
+      </aside>
+    </>
+  );
+};
+
+type SmallSidebarItemProps = {
+  Icon: ElementType;
+  title: string;
+  url: string;
+};
+
+type LargeSidebarSectionProps = {
+  children: React.ReactNode;
+  title?: string;
+  visibleItemCount?: number;
+};
+
+type LargeSidebarItemProps = {
+  Icon: ElementType;
+  title: string;
+  url: string;
+  isActive?: boolean;
+};
+
+function SmallSidebarItem({ Icon, title, url }: SmallSidebarItemProps) {
+  return (
+    <a
+      href={url}
+      className={twMerge(
+        buttonStyles({ variant: "ghost" }),
+        "px-1 py-4 flex flex-col items-center"
+      )}
+    >
+      <Icon className="h-6 w-6" />
+      <div className="text-sm">{title}</div>
+    </a>
+  );
 }
 
-export default Sidebar
+function LargeSidebarSection({
+  children,
+  title,
+  visibleItemCount = Number.POSITIVE_INFINITY,
+}: LargeSidebarSectionProps) {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const childrenArray = Children.toArray(children).flat();
+  const visibleChildren = isExpanded
+    ? childrenArray
+    : childrenArray.slice(0, visibleItemCount);
+  const showExpandButton = childrenArray.length > visibleItemCount;
+  const ButtonIcon = isExpanded ? ChevronUp : ChevronDown;
+
+  return (
+    <div>
+      {title && <div className="ml-4 mt-2 text-lg mb-1">{title}</div>}
+      {visibleChildren}
+      {showExpandButton && (
+        <Button
+          onClick={() => setIsExpanded(e => !e)}
+          variant="ghost"
+          className="w-full flex items-center rounded-lg gap-4 p-3"
+        >
+          <ButtonIcon className="w-6 h-6" />
+          <div>{isExpanded ? "Show Less" : "Show More"}</div>
+        </Button>
+      )}
+    </div>
+  );
+}
+
+function LargeSidebarItem({
+  Icon,
+  title,
+  url,
+  isActive,
+}: LargeSidebarItemProps) {
+  return (
+    <a
+      href={url}
+      className={twMerge(
+        buttonStyles({ variant: "ghost" }),
+        `w-full flex items-center rounded-lg gap-4 p-3 ${
+          isActive ? "font-bold bg-neutral-100 hover:bg-secondary" : ""
+        }`
+      )}
+    >
+      <Icon className="h-6 w-6" />
+      <div className="whitespace-nowrap overflow-hidden text-ellipsis">
+        {title}
+      </div>
+    </a>
+  );
+}
+
+export default Sidebar;
